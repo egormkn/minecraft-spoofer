12f82a014aebde9797737ae758ceff80b63b78f7
Egor Makarenko <egormkn@yandex.ru>
2020-04-01 16:49:14 +0300

    Remove static flags for OSX

--- scripts/build-osx.sh
+++ scripts/build-osx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir build
-clang main.cpp -Wall -std=c++17 -static -static -static-libstdc++ -static-libgcc -fno-exceptions -O3 -Wl,-s -o build/minecraft-spoofer-osx
+clang main.cpp -Wall -std=c++17 -fno-exceptions -O3 -Wl,-s -o build/minecraft-spoofer-osx
